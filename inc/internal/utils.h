08c8e9f61409d2c06e46f5739fdf13fd1a6578e5
Yava <yava.coco@gmail.com>
2022-11-22 10:36:54 +0100

    Remove DTC_SAFE_CALL
    * Calls should not have additional error filters

--- inc/internal/utils.h
+++ inc/internal/utils.h
@@ -5,10 +5,6 @@
 
 #define DTC_SET_OUT(out, val) if(out) *out = val
 
-#ifdef DTC_SAFE_CALL
 #define DTC_CALL(status_var, call) status_var = call; if(status_var)
-#else
-#define DTC_CALL(status_var, call) status_var = call; if(0)
-#endif
 
 #endif
